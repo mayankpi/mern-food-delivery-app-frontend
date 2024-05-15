79ea03fd9428483bb4d89e8ee50b9274c7203c6c
mayankpi <mayankm522@gmail.com>
2024-05-15 11:28:48 +0530

    add prepopulated profile form page

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,4 +1,22 @@
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 export default function UserProfilePage() {
-    return <UserProfileForm></UserProfileForm>;
+    const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+    const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+
+    if (isGetLoading) {
+        return <span>Loading...</span>;
+    }
+
+    if (!currentUser) {
+        return <span>Unable to load user profile</span>;
+    }
+
+    return (
+        <UserProfileForm
+            currentUser={currentUser}
+            onSave={updateUser}
+            isLoading={isUpdateLoading}
+        ></UserProfileForm>
+    );
 }
