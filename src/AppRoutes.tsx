429f963b0036bfebf9b6dba36b1ef5ef7b092175
mayankpi <mayankm522@gmail.com>
2024-05-17 01:24:11 +0530

    bugfix

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./layouts/layout";
+import Layout from "./layouts/Layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
@@ -10,63 +10,63 @@ import DetailPage from "./pages/DetailPage";
 import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout showHero>
-            <HomePage />
-          </Layout>
-        }
-      />
-      <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/search/:city"
-        element={
-          <Layout showHero={false}>
-            <SearchPage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/detail/:restaurantId"
-        element={
-          <Layout showHero={false}>
-            <DetailPage />
-          </Layout>
-        }
-      />
-      <Route element={<ProtectedRoute />}>
-        <Route
-          path="/order-status"
-          element={
-            <Layout>
-              <OrderStatusPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <Layout>
-              <UserProfilePage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/manage-restaurant"
-          element={
-            <Layout>
-              <ManageRestaurantPage />
-            </Layout>
-          }
-        />
-      </Route>
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <Layout showHero>
+                        <HomePage />
+                    </Layout>
+                }
+            />
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            <Route
+                path="/search/:city"
+                element={
+                    <Layout showHero={false}>
+                        <SearchPage />
+                    </Layout>
+                }
+            />
+            <Route
+                path="/detail/:restaurantId"
+                element={
+                    <Layout showHero={false}>
+                        <DetailPage />
+                    </Layout>
+                }
+            />
+            <Route element={<ProtectedRoute />}>
+                <Route
+                    path="/order-status"
+                    element={
+                        <Layout>
+                            <OrderStatusPage />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/user-profile"
+                    element={
+                        <Layout>
+                            <UserProfilePage />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/manage-restaurant"
+                    element={
+                        <Layout>
+                            <ManageRestaurantPage />
+                        </Layout>
+                    }
+                />
+            </Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
 };
 
 export default AppRoutes;
